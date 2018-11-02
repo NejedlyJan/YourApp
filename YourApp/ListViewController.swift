e95c61a57055ce0b06a638626bc3375fb8909612
honza <nejedly.jan@gmail.com>
2018-11-02 20:44:59 +0100

    table view setup

---
+++ YourApp/ListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  YourApp
+//
+//  Created by honza on 02/11/2018.
+//  Copyright © 2018 honza. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+    
+    let itemArray = ["Honza", "Daniel", "Marcus"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
