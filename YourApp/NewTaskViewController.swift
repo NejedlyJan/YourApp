93b4ed1d2aa441d5078d5a2d3b9be9eeddc40950
honza <nejedly.jan@gmail.com>
2018-11-02 21:31:13 +0100

    added assets

---
+++ YourApp/NewTaskViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewTaskViewController.swift
+//  YourApp
+//
+//  Created by honza on 02/11/2018.
+//  Copyright © 2018 honza. All rights reserved.
+//
+
+import UIKit
+
+class NewTaskViewController: UIViewController {
+    
+}
