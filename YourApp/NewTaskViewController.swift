e65d891497a8a31c62dacccfc9408744e6d995d0
honza <nejedly.jan@gmail.com>
2018-11-02 23:35:44 +0100

    CoreData setup

--- YourApp/NewTaskViewController.swift
+++ YourApp/NewTaskViewController.swift
@@ -7,7 +7,29 @@
 //
 
 import UIKit
+import CoreData
 
 class NewTaskViewController: UIViewController {
     
+    @IBOutlet weak var textFieldInput: UITextField!
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    
+    @IBAction func addItemButtonClicked(_ sender: UIButton) {
+        
+        let newItem = Task(context: self.context)
+        
+        newItem.done = false
+        newItem.title = textFieldInput.text
+        
+        saveItems()
+    }
+    func saveItems() {
+        do {
+            try context.save()
+            print("Saving to CoreData")
+        } catch {
+            print("Error saving data to CoreData: \(error)")
+        }
+    }
 }
