7e804fdbd7825fa84ac54dcee8bdcb64f920671f
honza <nejedly.jan@gmail.com>
2018-11-03 14:16:41 +0100

    added picker for category

--- YourApp/NewTaskViewController.swift
+++ YourApp/NewTaskViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 import CoreData
 
-class NewTaskViewController: UIViewController {
+class NewTaskViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
-    @IBOutlet weak var textFieldInput: UITextField!
+    @IBOutlet weak var titleInputOutlet: UITextField!
+    @IBOutlet weak var categoryInputOutlet: UITextField!
+    @IBOutlet weak var dueDateInputOutlet: UITextField!
+    @IBOutlet weak var categorySwitchOutlet: UISwitch!
+    @IBOutlet weak var dueDateSwitchOutlet: UISwitch!
+    
+    var pickerSelection = ["práce", "škola", "rodina"]
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     var selectedColorButtonTag = 1
     
     override func viewDidLoad() {
         selectButton()
+        
+        let pickerView  = UIPickerView()
+        pickerView.delegate = self
+        categoryInputOutlet.inputView = pickerView
+        
+        setupPickerToolbar()
     }
-    
     
     @IBAction func addItemButtonClicked(_ sender: UIButton) {
         
@@ -27,7 +38,7 @@ class NewTaskViewController: UIViewController {
         
         newItem.done = false
         newItem.categoryColor = Int32(selectedColorButtonTag)
-        newItem.title = textFieldInput.text
+        newItem.title = titleInputOutlet.text
         
         saveItems()
         navigationController?.popViewController(animated: true)
@@ -52,6 +63,39 @@ class NewTaskViewController: UIViewController {
             }
         }
     }
+    
+    fileprivate func setupPickerToolbar() {
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor.black
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneClicked))
+        let spacingButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolBar.setItems([spacingButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        categoryInputOutlet.inputAccessoryView = toolBar
+    }
+    
+    @objc func doneClicked() {
+        categoryInputOutlet.resignFirstResponder()
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+       return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerSelection.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerSelection[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        categoryInputOutlet.text = pickerSelection[row]
+    }
+    
     
     func saveItems() {
         do {
