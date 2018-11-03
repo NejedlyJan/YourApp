31cdeb5a93281d3e095bc93278ce7eef4d29208b
honza <nejedly.jan@gmail.com>
2018-11-03 10:34:16 +0100

    Basic saving and fetching

--- YourApp/ListViewController.swift
+++ YourApp/ListViewController.swift
@@ -7,41 +7,52 @@
 //
 
 import UIKit
+import CoreData
 
 class ListViewController: UITableViewController {
     
-    let itemArray = ["Honza", "Daniel", "Marcus"]
-
+    var itemArray = [Task]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        loadItems()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(itemArray[indexPath.row])
-        
-        
+
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         }
         else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
-        
         tableView.deselectRow(at: indexPath, animated: true)
     }
-
+    func loadItems() {
+        let request : NSFetchRequest<Task> = Task.fetchRequest()
+        do {
+            itemArray = try context.fetch(request)
+        }
+        catch {
+            print("Error fetching data \(error)")
+        }
+    }
+    
+    
 }
 
